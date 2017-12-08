e42597008b5babb317bb32dbec14f752d55377e6
dube1503 <dube1503@gmail.com>
2017-12-08 15:19:51 -0600

    directory upload

---
+++ js/actions.js
@@ -0,0 +1,54 @@
+$(function() {
+    var cart = $('li.cart');
+    var overlay = $('#contentWrap');
+    var order = $('#order');
+    var payment = $('section.filing div.billing form');
+    var addCart = $('button#addtocart');
+ 
+    cart.click(function() {
+        if (overlay.is(':empty')) {
+            overlay.load('/payment/cart.html #background-overlay');
+        };
+
+    });
+
+    addCart.click(function() {
+        if (overlay.is(':empty')) {
+            overlay.load('/payment/cart.html #background-overlay');
+        };
+
+    });
+
+    overlay.on('click', '#exit', function () {
+        overlay.empty();
+    });
+
+    order.click(function() {
+        if (overlay.is(':empty')) {
+            overlay.load('/payment/purchase.html #background-overlay');
+        };
+    });
+
+    overlay.on('change', '#plate-quantity', function () {
+        var plateQty = parseInt($("#plate-quantity select").val());
+        var cupQty = parseInt($("#cup-quantity select").val());
+
+        var platePrice = Math.floor(plateQty*24.99*100)/100;
+        var cupPrice = Math.floor(cupQty*18.99*100)/100;
+
+        $(".plate .item-price").text("$"+platePrice);
+        $("#total-price .final").text("$"+(Math.floor((cupPrice+platePrice+12.97)*100)/100));
+    });
+
+    overlay.on('change', '#cup-quantity', function () {
+        var plateQty = parseInt($("#plate-quantity select").val());
+        var cupQty = parseInt($("#cup-quantity select").val());
+
+        var platePrice = Math.floor(plateQty*24.99*100)/100;
+        var cupPrice = Math.floor(cupQty*18.99*100)/100;
+
+        $(".cup .item-price").text("$"+cupPrice);
+        $("#total-price .final").text("$"+(Math.floor((cupPrice+platePrice+12.97)*100)/100));
+    });
+            
+});
